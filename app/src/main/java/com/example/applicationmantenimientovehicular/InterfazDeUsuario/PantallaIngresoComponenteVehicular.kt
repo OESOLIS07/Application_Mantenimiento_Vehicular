8d8db0f36afceb9a297f54ce86dbbc38c2c2871f
OESOLIS07 <157768093+OESOLIS07@users.noreply.github.com>
2025-04-06 18:24:30 -0600

    Configurando Pantalla de Ingreso de Componentes

--- app/src/main/java/com/example/applicationmantenimientovehicular/InterfazDeUsuario/PantallaIngresoComponenteVehicular.kt
+++ app/src/main/java/com/example/applicationmantenimientovehicular/InterfazDeUsuario/PantallaIngresoComponenteVehicular.kt
@@ -5,8 +5,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -20,6 +25,9 @@ fun IngresoDeComponentes(
     viewModel: AceiteMotViewModel = hiltViewModel()
 )
 {
+    var marca by remember { mutableStateOf("") } // Estado para la Marca del Producto
+    var durabilidad by remember { mutableStateOf("") } // Estado para la Durabilidad del Producto
+    var textFieldVisible by remember { mutableStateOf(false) }
     Column (
         modifier = Modifier
             .fillMaxSize()
@@ -27,6 +35,32 @@ fun IngresoDeComponentes(
     )
     {
         Text("Pantalla Ingreso de Componentes")
+        // Boton para seleccionar el Aceite
+        Button(
+            onClick = {
+                textFieldVisible = true
+            },
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+                .fillMaxWidth()
+        ) {
+            Text("Aceite")
+            //Text(if (textFieldEnabled) "Desactivar TextField" else "Activar TextField")
+        }
+        if (textFieldVisible) {
+            OutlinedTextField(
+                value = marca,
+                onValueChange = { marca= it },
+                label = { Text("Ingrese la Marca del Producto") },
+                modifier = Modifier.fillMaxWidth()
+            )
+            OutlinedTextField(
+                value = durabilidad,
+                onValueChange = { durabilidad= it },
+                label = { Text("Ingrese la Durabilidad del Producto") },
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
         //Boton para Guardar el nuevo Componente
         Button(
             onClick = {
